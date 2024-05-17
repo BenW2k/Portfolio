615921a79544fbbd74a94ce69ece2d54dec3efb6
BenW2k <benworkman16@gmail.com>
2024-05-17 19:47:37 +0100

    +hero-section html

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,5 +2,12 @@ import Image from "next/image";
 import styles from "./styles/pages/home.module.css";
 
 export default function Home() {
-  return <div className={styles["hero-section"]}>Hello World</div>;
+  return (
+    <div className={styles["hero-section"]}>
+      <h1 className={styles["hero-title"]}>Hello</h1>
+      <p className={styles["hero-paragraph"]}>
+        I'm Ben Workman, a software engineer
+      </p>
+    </div>
+  );
 }
