7dfd6cefb6828e58c04f00a99240b3d7b9202c1b
BenW2k <benworkman16@gmail.com>
2024-05-18 16:28:27 +0100

    +additions to hero paragraph

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,8 @@ export default function Home() {
     <div className={styles["hero-section"]}>
       <h1 className={styles["hero-title"]}>Hello</h1>
       <p className={styles["hero-paragraph"]}>
-        I'm Ben Workman, a software engineer
+        I'm Ben Workman, a Full-stack Software Engineer with sub-specialties in
+        Web Development, Data Science, Machine Learning and Finance.
       </p>
     </div>
   );
