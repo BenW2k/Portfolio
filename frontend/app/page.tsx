ef471d2b8f37984284db7c9bb184e4add6f2625b
BenW2k <benworkman16@gmail.com>
2024-05-19 17:59:27 +0100

    +useAnimate const

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import styles from "./styles/pages/home.module.css";
+import {motion, useAnimate} from "framer-motion";
 
 export default function Home() {
+  const [scope, animate] = useAnimate();
+
   return (
-    <div className={styles["hero-section"]}>
+    <motion.div ref={scope} className={styles["hero-section"]}>
       <h1 className={styles["hero-title"]}>Hello</h1>
       <p className={styles["hero-paragraph"]}>
         I'm Ben Workman, a Full-stack Software Engineer with sub-specialties in
         Web Development, Data Science, Machine Learning and Finance.
       </p>
-    </div>
+    </motion.div>
   );
 }
