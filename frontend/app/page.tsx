6cba28840c6c58c36f00458d4cedc8e15bb1c3f2
BenW2k <benworkman16@gmail.com>
2024-05-20 18:02:09 +0100

    +framer motion

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./styles/pages/home.module.css";
 import {motion, useAnimate} from "framer-motion";
@@ -5,13 +7,25 @@ import {motion, useAnimate} from "framer-motion";
 export default function Home() {
   const [scope, animate] = useAnimate();
 
+  const handleAnimate = async () => {
+    animate("#hero-title", {opacity: 1}, {duration: 0.5});
+    // animate("#hero-paragraph", {opacity: 1});
+    // animate("#hero-paragraph", {opacity: 1});
+  };
+
   return (
-    <motion.div ref={scope} className={styles["hero-section"]}>
-      <h1 className={styles["hero-title"]}>Hello</h1>
-      <p className={styles["hero-paragraph"]}>
+    <div
+      ref={scope}
+      onLoad={() => handleAnimate()}
+      className={styles["hero-section"]}
+    >
+      <h1 id="hero-title" className={styles["hero-title"]}>
+        Hello
+      </h1>
+      <p id="hero-paragraph" className={styles["hero-paragraph"]}>
         I'm Ben Workman, a Full-stack Software Engineer with sub-specialties in
         Web Development, Data Science, Machine Learning and Finance.
       </p>
-    </motion.div>
+    </div>
   );
 }
