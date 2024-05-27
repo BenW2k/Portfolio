2f5c237b31b3fd1aa999cf5fc0643ba53cdcdaae
BenW2k <benworkman16@gmail.com>
2024-05-27 22:35:51 +0100

    +imports and span animations

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,14 @@ import Navbar from "./components/navbar";
 import {useEffect} from "react";
 import Image from "next/image";
 import styles from "./styles/pages/home.module.css";
-import {easeInOut, motion, useAnimate, useInView, stagger} from "framer-motion";
+import {
+  easeInOut,
+  motion,
+  useAnimate,
+  useInView,
+  stagger,
+  delay,
+} from "framer-motion";
 
 export default function Home() {
   const [scope, animate] = useAnimate();
@@ -18,9 +25,38 @@ export default function Home() {
           {duration: 1, ease: "easeInOut"}
         );
         // await animate(scope.current, {y: -100}, {duration: 1});
-        await animate("#hero-paragraph", {opacity: 0.6}, {duration: 1});
-        // await animate("#hero-tag", {opacity: 1}, {duration: 2});
-        await animate("#navbar-div", {opacity: 1}, {duration: 2});
+        await animate("#hero-paragraph", {opacity: 1}, {duration: 1});
+        await animate("#hero-span-1", {color: "#ffffff"}, {duration: 0.25});
+        await animate(
+          "#hero-span-1",
+          {color: "#ffffff99"},
+          {duration: 0.5, delay: 0.75}
+        );
+        await animate("#hero-span-2", {color: "#ffffff"}, {duration: 0.25});
+        await animate(
+          "#hero-span-2",
+          {color: "#ffffff99"},
+          {duration: 0.25, delay: 0.75}
+        );
+        await animate("#hero-span-3", {color: "#ffffff"}, {duration: 0.25});
+        await animate(
+          "#hero-span-3",
+          {color: "#ffffff99"},
+          {duration: 0.25, delay: 0.75}
+        );
+        await animate("#hero-span-4", {color: "#ffffff"}, {duration: 0.25});
+        await animate(
+          "#hero-span-4",
+          {color: "#ffffff99"},
+          {duration: 0.25, delay: 0.75}
+        );
+        await animate("#hero-span-5", {color: "#ffffff"}, {duration: 0.25});
+        await animate(
+          "#hero-span-5",
+          {color: "#ffffff99"},
+          {duration: 0.25, delay: 0.75}
+        );
+        await animate("#navbar-div", {opacity: 1}, {duration: 1});
       };
       heroAnimation();
     }
@@ -36,10 +72,12 @@ export default function Home() {
           Hello
         </h1>
         <p id="hero-paragraph" className={styles["hero-paragraph"]}>
-          I'm Ben Workman, a{" "}
-          <span id="hero-tag">Full-stack Software Engineer</span> with
-          sub-specialties in Web Development, Data Science, Machine Learning and
-          Finance.
+          I'm <span id="name-span">Ben Workman</span>, a{" "}
+          <span id="hero-span-1">Full-stack Software Engineer</span> with
+          sub-specialties in <span id="hero-span-2">Web Development</span>,{" "}
+          <span id="hero-span-3">Data Science</span>,{" "}
+          <span id="hero-span-4">Machine Learning</span> and
+          <span id="hero-span-5"> Finance.</span>
         </p>
       </div>
     </div>
