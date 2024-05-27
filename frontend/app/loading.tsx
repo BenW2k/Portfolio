21e9144568c93cb9cc1b21572e7db0c451fb5e2c
BenW2k <benworkman16@gmail.com>
2024-05-27 02:50:37 +0100

    +loading animation

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,1 +1,9 @@
-import styles
+import styles from "./styles/pages/loading.module.css";
+
+export default function Loading() {
+  <div className="loader">
+    <span className={styles["bar"]}></span>
+    <span className={styles["bar"]}></span>
+    <span className={styles["bar"]}></span>
+  </div>;
+}
