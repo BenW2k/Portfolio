9ea9a1c8585e95f3ea6195a0d241788f26636c6c
BenW2k <benworkman16@gmail.com>
2024-05-27 02:53:08 +0100

    +fixed an issue with the return statement

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,9 +1,11 @@
 import styles from "./styles/pages/loading.module.css";
 
 export default function Loading() {
-  <div className="loader">
-    <span className={styles["bar"]}></span>
-    <span className={styles["bar"]}></span>
-    <span className={styles["bar"]}></span>
-  </div>;
+  return (
+    <div className="loader">
+      <span className={styles["bar"]}></span>
+      <span className={styles["bar"]}></span>
+      <span className={styles["bar"]}></span>
+    </div>
+  );
 }
