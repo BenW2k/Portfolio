8385018b3c3695ef6bfef7176f3d6aefdcc5a62b
BenW2k <benworkman16@gmail.com>
2024-05-28 15:05:57 +0100

    +timeline import and commented out animation (for now) to avoid animation on hot refresh

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -17,52 +17,51 @@ export default function Home() {
   const [scope, animate] = useAnimate();
   const isInView = useInView(scope);
 
-  useEffect(() => {
-    if (isInView) {
-      const heroAnimation = async () => {
-        await animate(
-          "#hero-title",
-          {opacity: 1},
-          {duration: 1, ease: "easeInOut"}
-        );
-        // await animate(scope.current, {y: -100}, {duration: 1});
-        await animate("#hero-paragraph", {opacity: 1}, {duration: 1});
-        await animate("#hero-span-1", {color: "#ffffff"}, {duration: 0.25});
-        await animate(
-          "#hero-span-1",
-          {color: "#ffffff99"},
-          {duration: 0.5, delay: 0.75}
-        );
-        await animate("#hero-span-2", {color: "#ffffff"}, {duration: 0.25});
-        await animate(
-          "#hero-span-2",
-          {color: "#ffffff99"},
-          {duration: 0.25, delay: 0.75}
-        );
-        await animate("#hero-span-3", {color: "#ffffff"}, {duration: 0.25});
-        await animate(
-          "#hero-span-3",
-          {color: "#ffffff99"},
-          {duration: 0.25, delay: 0.75}
-        );
-        await animate("#hero-span-4", {color: "#ffffff"}, {duration: 0.25});
-        await animate(
-          "#hero-span-4",
-          {color: "#ffffff99"},
-          {duration: 0.25, delay: 0.75}
-        );
-        await animate("#hero-span-5", {color: "#ffffff"}, {duration: 0.25});
-        await animate(
-          "#hero-span-5",
-          {color: "#ffffff99"},
-          {duration: 0.25, delay: 0.75}
-        );
-        await animate("#name-span", {color: "#66CCCC"}, {duration: 1});
-        await animate("#navbar-div", {opacity: 1}, {duration: 1});
-      };
-      heroAnimation();
-    }
-  }, [isInView]);
+  // useEffect(() => {
+  //   if (isInView) {
+  //     const heroAnimation = async () => {
+  //       await animate(
+  //         "#hero-title",
+  //         {opacity: 1},
+  //         {duration: 1, ease: "easeInOut"}
+  //       );
+  //       await animate("#hero-paragraph", {opacity: 1}, {duration: 1});
+  //       await animate("#hero-span-1", {color: "#ffffff"}, {duration: 0.25});
+  //       await animate(
+  //         "#hero-span-1",
+  //         {color: "#ffffff99"},
+  //         {duration: 0.5, delay: 0.75}
+  //       );
+  //       await animate("#hero-span-2", {color: "#ffffff"}, {duration: 0.25});
+  //       await animate(
+  //         "#hero-span-2",
+  //         {color: "#ffffff99"},
+  //         {duration: 0.25, delay: 0.75}
+  //       );
+  //       await animate("#hero-span-3", {color: "#ffffff"}, {duration: 0.25});
+  //       await animate(
+  //         "#hero-span-3",
+  //         {color: "#ffffff99"},
+  //         {duration: 0.25, delay: 0.75}
+  //       );
+  //       await animate("#hero-span-4", {color: "#ffffff"}, {duration: 0.25});
+  //       await animate(
+  //         "#hero-span-4",
+  //         {color: "#ffffff99"},
+  //         {duration: 0.25, delay: 0.75}
+  //       );
+  //       await animate("#hero-span-5", {color: "#ffffff"}, {duration: 0.25});
+  //       await animate(
+  //         "#hero-span-5",
+  //         {color: "#ffffff99"},
+  //         {duration: 0.25, delay: 0.75}
+  //       );
+  //       await animate("#name-span", {color: "#66CCCC"}, {duration: 1});
+  //       await animate("#navbar-div", {opacity: 1}, {duration: 1});
+  //     };
+  //     heroAnimation();
+  //   }
+  // }, [isInView]);
 
   return (
     <div ref={scope}>
@@ -81,6 +80,9 @@ export default function Home() {
           <span id="hero-span-4">Machine Learning</span> and
           <span id="hero-span-5"> Finance.</span>
         </p>
+      </div>
+      <div className={styles["timeline"]}>
+        <Timeline />
       </div>
     </div>
   );
