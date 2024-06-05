a595248c7278d0323297eda8822c81e8c84472a7
BenW2k <benworkman16@gmail.com>
2024-06-05 15:16:56 +0100

    +data array

--- frontend/app/components/skills.tsx
+++ frontend/app/components/skills.tsx
@@ -1,12 +1,31 @@
 import styles from "../styles/components/skills.module.css";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function Skills() {
+  const data = [
+    {
+      name: "Software Development",
+      answer: "lorem ipsum 1",
+    },
+    {
+      name: "Machine Learning",
+      answer: "lorem ipsum 2",
+    },
+    {
+      name: "Data Science",
+      answer: "lorem ipsum 3",
+    },
+    {
+      name: "Other",
+      answer: "lorem ipsum 4",
+    },
+  ];
   return (
     <div className={styles["skills"]}>
       <div className={styles["dummy-container"]}>
-        <div className={styles["skills-heading"]}>
+        <button className={styles["skills-title"]}>
           <h1>Skills</h1>
-        </div>
+        </button>
         <div className={styles["skill-panel"]}>
           <h1>Software Development</h1>
         </div>
