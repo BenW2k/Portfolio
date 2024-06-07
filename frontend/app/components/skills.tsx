a62cbad80fbe82be1778d1a4ebb66aa6859c93a9
BenW2k <benworkman16@gmail.com>
2024-06-07 16:08:14 +0100

    +accordion now functions (albeit without transitions)

--- frontend/app/components/skills.tsx
+++ frontend/app/components/skills.tsx
@@ -1,7 +1,13 @@
 import styles from "../styles/components/skills.module.css";
-import useState from "react";
+import {useState} from "react";
 
 export default function Skills() {
+  const [val, setVal] = useState(1);
+
+  const handleClick = (num: number) => {
+    setVal(num); // Update state on click
+  };
+
   return (
     <div className={styles["skills"]}>
       <div className={styles["dummy-container"]}>
@@ -9,11 +15,27 @@ export default function Skills() {
           <h1>Skills</h1>
         </div>
         <div className={styles["skill-panel"]}>
-          <button className={styles["skill-title"]}>
+          <button
+            className={styles["skill-title"]}
+            onClick={() => {
+              if (val === 2) {
+                setVal(1);
+              } else {
+                setVal(2);
+              }
+              console.log(val);
+            }}
+          >
             <h1>Software Development</h1>
-            <span>+</span>
+            <span>{val === 2 ? "-" : "+"}</span>
           </button>
-          <div className={styles["skills-body"]}>
+          <div
+            className={
+              val === 2
+                ? `${styles["skills-body"]} ${styles["show"]}`
+                : styles["hidden"]
+            }
+          >
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo id
               est asperiores, consequuntur, praesentium perspiciatis beatae
@@ -22,14 +44,89 @@ export default function Skills() {
             </p>
           </div>
         </div>
-        <div className={styles["skill-panel"]}>
+        <button
+          className={styles["skill-title"]}
+          onClick={() => {
+            if (val === 3) {
+              setVal(1);
+            } else {
+              setVal(3);
+            }
+            console.log(val);
+          }}
+        >
           <h1>Machine Learning</h1>
+          <span>{val === 3 ? "-" : "+"}</span>
+        </button>
+        <div
+          className={
+            val === 3
+              ? `${styles["skills-body"]} ${styles["show"]}`
+              : styles["hidden"]
+          }
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo id est
+            asperiores, consequuntur, praesentium perspiciatis beatae voluptatem
+            incidunt aut quo iusto quae magni vitae architecto, cum aliquid
+            minus inventore. Laboriosam!
+          </p>
         </div>
-        <div className={styles["skill-panel"]}>
+        <button
+          className={styles["skill-title"]}
+          onClick={() => {
+            if (val === 4) {
+              setVal(1);
+            } else {
+              setVal(4);
+            }
+            console.log(val);
+          }}
+        >
           <h1>Data Science</h1>
+          <span>{val === 4 ? "-" : "+"}</span>
+        </button>
+        <div
+          className={
+            val === 4
+              ? `${styles["skills-body"]} ${styles["show"]}`
+              : styles["hidden"]
+          }
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo id est
+            asperiores, consequuntur, praesentium perspiciatis beatae voluptatem
+            incidunt aut quo iusto quae magni vitae architecto, cum aliquid
+            minus inventore. Laboriosam!
+          </p>
         </div>
-        <div className={styles["skill-panel"]}>
+        <button
+          className={styles["skill-title"]}
+          onClick={() => {
+            if (val === 5) {
+              setVal(1);
+            } else {
+              setVal(5);
+            }
+            console.log(val);
+          }}
+        >
           <h1>Other</h1>
+          <span>{val === 5 ? "-" : "+"}</span>
+        </button>
+        <div
+          className={
+            val === 5
+              ? `${styles["skills-body"]} ${styles["show"]}`
+              : styles["hidden"]
+          }
+        >
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo id est
+            asperiores, consequuntur, praesentium perspiciatis beatae voluptatem
+            incidunt aut quo iusto quae magni vitae architecto, cum aliquid
+            minus inventore. Laboriosam!
+          </p>
         </div>
       </div>
     </div>
