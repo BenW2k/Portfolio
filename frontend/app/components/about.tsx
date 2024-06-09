ba1655ba65c0b77ef4a85b599afe08e33ce96217
BenW2k <benworkman16@gmail.com>
2024-06-09 12:31:09 +0100

    +basic structure

--- frontend/app/components/about.tsx
+++ frontend/app/components/about.tsx
@@ -1,5 +1,27 @@
 import styles from "../styles/components/about.module.css";
 
 export default function About() {
-  return <div></div>;
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["grid"]}>
+        <div className={styles["intro"]}>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            fugit soluta non expedita modi minima repellat. Quod non error qui
+            totam labore sit hic cumque mollitia molestiae, quae perferendis
+            architecto?
+          </p>
+        </div>
+        <div className={styles["ml-and-ds"]}>
+          <h3>Machine Learning & Data Science</h3>
+        </div>
+        <div className={styles["dev"]}>
+          <h3>Web & Software Development</h3>
+        </div>
+        <div className={styles["face"]}></div>
+        <p>yes</p>
+      </div>
+    </div>
+  );
 }
