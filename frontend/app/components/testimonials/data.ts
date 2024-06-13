1533f920296f8194b36a1f2338db92f289dc9b50
BenW2k <benworkman16@gmail.com>
2024-06-13 15:35:51 +0100

    +testimonial data

---
+++ frontend/app/components/testimonials/data.ts
@@ -0,0 +1,25 @@
+import placeholder from "../../assets/media/placeholder-image.svg";
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    img: {placeholder},
+    color: "#88S28D",
+    person: "Synergy Construction",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    img: {placeholder},
+    color: "BBACAF",
+    person: "Connor Larkin - Glasgow Caledonian University",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    img: {placeholder},
+    color: "#C2491D",
+    person: "Placeholder Name",
+  },
+];
