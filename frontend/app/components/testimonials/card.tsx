e95605177e9c9d0b303016492465316dac6ae120
BenW2k <benworkman16@gmail.com>
2024-06-20 07:57:33 +0100

    +interface and typing instantiation

---
+++ frontend/app/components/testimonials/card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "../../styles/components/testimonials/card.module.css";
+import Image from "next/image";
+
+interface Card {
+  quote: string;
+  img: string;
+  color: string;
+  person: string;
+}
+
+export default function Card(card: Card) {
+  return (
+    <div className={styles.cardContainer}>
+      <p>{card.quote}</p>
+      <Image src={card.img} alt="card-img" />
+    </div>
+  );
+}
