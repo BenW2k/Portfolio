df775c558ee24b0d3d4ec4f124530af388e486b9
BenW2k <benworkman16@gmail.com>
2024-06-26 14:05:47 +0100

    +framer motion logic and additional params

--- frontend/app/components/testimonials/testimonials.tsx
+++ frontend/app/components/testimonials/testimonials.tsx
@@ -1,12 +1,28 @@
 import {testimonials} from "./data";
 import styles from "../../styles/components/testimonials/testimonials.module.css";
 import Card from "./card";
+import {motion, useScroll} from "framer-motion";
+import {useRef} from "react";
 
 export default function Testimonials() {
+  const container = useRef(null);
+  const {scrollYProgress} = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
   return (
-    <main className={styles.main}>
+    <main ref={container} className={styles.main}>
       {testimonials.map((testimonial, index) => {
-        return <Card key={index} {...testimonial} />;
+        const targetScale = 1 - (testimonials.length - index) * 0.05;
+        return (
+          <Card
+            key={index}
+            {...testimonial}
+            range={[testimonial.i * 0.33, 1]}
+            progress={scrollYProgress}
+            targetScale={targetScale}
+          />
+        );
       })}
     </main>
   );
