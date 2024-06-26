8e261985c251301f4c4b6e58a1c85af59a2d9707
BenW2k <benworkman16@gmail.com>
2024-06-26 19:12:29 +0100

    +colour changes

--- frontend/app/components/testimonials/data.ts
+++ frontend/app/components/testimonials/data.ts
@@ -5,16 +5,16 @@ export const testimonials = [
     quote:
       "First ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
     img: placeholder,
-    color: "blue",
+    color: "rgba(232, 236, 255, 1)",
     person: "Mark",
     company: "Synergy Construction",
     i: 1,
   },
   {
     quote:
-      "Second ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      "Ben provided great communication throughout the project and always had my interests in mind. Alongside this, he completed the work in a very timely manner and allowed for apt time for revisions. Ben's professionalism and passion made him a joy to work with.",
     img: placeholder,
-    color: "#BBACAF",
+    color: "rgba(216, 223, 255, 1)",
     person: "Connor Larkin ",
     company: "Glasgow Caledonian University",
     i: 2,
@@ -23,7 +23,7 @@ export const testimonials = [
     quote:
       "Third ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
     img: placeholder,
-    color: "#C2491D",
+    color: "rgba(195, 205, 255, 1)",
     person: "Placeholder Name",
     company: "Placeholder Company",
     i: 3,
