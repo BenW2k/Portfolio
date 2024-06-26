c5d4fab579b1d68346988d81a16a4ee9dacd37e7
BenW2k <benworkman16@gmail.com>
2024-06-26 15:30:13 +0100

    +index and dummy data

--- frontend/app/components/testimonials/data.ts
+++ frontend/app/components/testimonials/data.ts
@@ -3,23 +3,29 @@ import placeholder from "../../assets/media/placeholder-image.svg";
 export const testimonials = [
   {
     quote:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    img: {placeholder},
-    color: "#88S28D",
-    person: "Synergy Construction",
+      "First ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    img: placeholder,
+    color: "blue",
+    person: "Mark",
+    company: "Synergy Construction",
+    i: 1,
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    img: {placeholder},
-    color: "BBACAF",
-    person: "Connor Larkin - Glasgow Caledonian University",
+      "Second ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    img: placeholder,
+    color: "#BBACAF",
+    person: "Connor Larkin ",
+    company: "Glasgow Caledonian University",
+    i: 2,
   },
   {
     quote:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    img: {placeholder},
+      "Third ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    img: placeholder,
     color: "#C2491D",
     person: "Placeholder Name",
+    company: "Placeholder Company",
+    i: 3,
   },
 ];
