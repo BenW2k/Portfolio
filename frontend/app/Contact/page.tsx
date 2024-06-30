041766f8b223dfa0790415295281363724566583
BenW2k <benworkman16@gmail.com>
2024-06-30 15:39:59 +0100

    -moved scene logic to separate component

--- frontend/app/Contact/page.tsx
+++ frontend/app/Contact/page.tsx
@@ -1,14 +1,13 @@
-import {Canvas} from "@react-three/fiber";
-import {Environment} from "@react-three/drei";
-import {Model} from "../components/contact/glass";
+import dynamic from "next/dynamic";
 
-export default function Scene() {
-  <Canvas
-    orthographic
-    style={{backgroundColor: "black"}}
-    camera={{position: [0, 0, 1], zoom: 800}}
-  >
-    <directionalLight intensity={3} position={[0, 0.1, 1]} />
-    <Model />
-  </Canvas>;
+const Scene = dynamic(() => import("@/app/components/contact/Scene"), {
+  ssr: false,
+});
+
+export default function Contact() {
+  return (
+    <main className="relative h-screen">
+      <Scene />
+    </main>
+  );
 }
