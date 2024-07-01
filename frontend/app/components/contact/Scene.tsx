e84b13d397212ad01fd588787db9b9eacfe3b0d9
BenW2k <benworkman16@gmail.com>
2024-07-01 13:45:29 +0100

    +styling

--- frontend/app/components/contact/Scene.tsx
+++ frontend/app/components/contact/Scene.tsx
@@ -2,22 +2,18 @@
 
 import {Canvas} from "@react-three/fiber";
 import {Environment} from "@react-three/drei";
-import {Model} from "./glass";
-import dynamic from "next/dynamic";
+import Model from "./glass";
 
 export default function Scene() {
   return (
-    <div style={{width: "100vw", height: "100vh"}}>
-      <Canvas
-        orthographic
-        style={{backgroundColor: "black"}}
-        camera={{position: [-5, 20, -5], zoom: 3200}}
-      >
-        <Model />
-        <directionalLight intensity={3} position={[1, 0.1, 1]} />
-        <Environment preset="city" />
-      </Canvas>
-      ;
-    </div>
+    <Canvas
+      orthographic
+      style={{background: "black", width: "100vw", height: "100vh"}}
+      camera={{position: [0, 0, 1], zoom: 800}}
+    >
+      <Model />
+      <directionalLight intensity={3} position={[0, 0.1, 1]} />
+      <Environment preset="city" />
+    </Canvas>
   );
 }
