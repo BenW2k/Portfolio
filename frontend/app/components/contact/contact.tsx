d6a501b5ecd39fdec7f65188b02e4ed4832e98f8
BenW2k <benworkman16@gmail.com>
2024-07-02 13:47:22 +0100

    +basic form logic

--- frontend/app/components/contact/contact.tsx
+++ frontend/app/components/contact/contact.tsx
@@ -1,5 +1,8 @@
 import dynamic from "next/dynamic";
 import styles from "../../styles/components/contact.module.css";
+import {useState, useRef} from "react";
+import emailjs from "@emailjs/browser";
+import {formKey} from "./keys";
 
 const Scene = dynamic(() => import("@/app/components/contact/Scene"), {
   ssr: false,
@@ -11,6 +14,15 @@ export default function Contact() {
       <Scene />
       <div className={styles.contactForm}>
         <h2 className={styles.title}>Contact Me</h2>
+        <form className={styles.form}>
+          <input type="text" className={styles.field} placeholder="Your Name" />
+          <input
+            type="text"
+            className={styles.field}
+            placeholder="example@gmail.com"
+          />
+          <input type="text" className={styles.field} placeholder="..." />
+        </form>
       </div>
     </main>
   );
