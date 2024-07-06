f8f9c49901ee99e90425191cf9bab8ddf7d751d3
BenW2k <benworkman16@gmail.com>
2024-07-06 21:33:54 +0100

    +initial commit

---
+++ frontend/app/components/projects/project.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./style.module.css";
+
+interface props {
+  index: number;
+  title: string;
+  setModal: any;
+  type: string;
+}
+
+export default function Project({index, title, setModal, type}: props) {
+  return (
+    <div
+      onMouseEnter={() => {
+        setModal({active: true, index});
+      }}
+      onMouseLeave={() => {
+        setModal({active: false, index});
+      }}
+      className={styles.project}
+    >
+      <h2>{title}</h2>
+
+      <p>{type}</p>
+    </div>
+  );
+}
