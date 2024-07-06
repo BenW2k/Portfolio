16c790528f44e24628ddd831f65f8a026c4a2910
BenW2k <benworkman16@gmail.com>
2024-07-06 21:34:05 +0100

    +initial commit

--- frontend/app/components/projects/projects.tsx
+++ frontend/app/components/projects/projects.tsx
@@ -1,3 +1,72 @@
+import styles from "../../styles/components/projects/projects.module.css";
+import Image from "next/image";
+import Project from "./project";
+import Modal from "./modal";
+import place from "../../assets/media/placeholder-image.svg";
+import {useScroll, useTransform, motion} from "framer-motion";
+import {useRef, useEffect, useState} from "react";
+
 export default function Projects() {
-  return <p>Hello World</p>;
+  const projects = [
+    {
+      title: "Synergy",
+
+      src: {place},
+
+      color: "#000000",
+
+      type: "Fullstack Development",
+    },
+
+    {
+      title: "Client Portfolio",
+
+      src: {place},
+
+      color: "#8C8C8C",
+
+      type: "Frontend Development",
+    },
+
+    {
+      title: "Facial Recognition",
+
+      src: {place},
+
+      color: "#EFE8D3",
+
+      type: "Machine Learning",
+    },
+
+    {
+      title: "Unsure",
+
+      src: {place},
+
+      color: "#706D63",
+      type: "Machine Learning",
+    },
+  ];
+
+  const [modal, setModal] = useState({active: false, index: 0});
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.body}>
+        {projects.map((project, index) => {
+          return (
+            <Project
+              index={index}
+              title={project.title}
+              setModal={setModal}
+              key={index}
+              type={project.type}
+            />
+          );
+        })}
+      </div>
+
+      <Modal modal={modal} projects={projects} />
+    </main>
+  );
 }
