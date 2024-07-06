a48a7c63560cf2eb18cf0db4b90265e54fb97471
BenW2k <benworkman16@gmail.com>
2024-07-06 23:59:30 +0100

    +gsap follow cursor logic

--- frontend/app/components/projects/modal.tsx
+++ frontend/app/components/projects/modal.tsx
@@ -1,7 +1,8 @@
-import {useRef} from "react";
+import {useRef, useEffect} from "react";
 import {motion} from "framer-motion";
 import Image from "next/image";
-import styles from "./style.module.css";
+import styles from "../../styles/components/projects/modal.module.css";
+import gsap from "gsap";
 
 const scaleAnimation = {
   initial: {scale: 0, x: "-50%", y: "-50%"},
@@ -27,9 +28,70 @@ interface props {
 export default function Modal({modal, projects}: props) {
   const {active, index} = modal;
 
+  const modalContainer = useRef(null);
+
+  const cursor = useRef(null);
+
+  const cursorLabel = useRef(null);
+
+  useEffect(() => {
+    //Move Container
+
+    let xMoveContainer = gsap.quickTo(modalContainer.current, "left", {
+      duration: 0.8,
+      ease: "power3",
+    });
+
+    let yMoveContainer = gsap.quickTo(modalContainer.current, "top", {
+      duration: 0.8,
+      ease: "power3",
+    });
+
+    //Move cursor
+
+    let xMoveCursor = gsap.quickTo(cursor.current, "left", {
+      duration: 0.5,
+      ease: "power3",
+    });
+
+    let yMoveCursor = gsap.quickTo(cursor.current, "top", {
+      duration: 0.5,
+      ease: "power3",
+    });
+
+    //Move cursor label
+
+    let xMoveCursorLabel = gsap.quickTo(cursorLabel.current, "left", {
+      duration: 0.45,
+      ease: "power3",
+    });
+
+    let yMoveCursorLabel = gsap.quickTo(cursorLabel.current, "top", {
+      duration: 0.45,
+      ease: "power3",
+    });
+
+    window.addEventListener("mousemove", (e) => {
+      const {pageX, pageY} = e;
+
+      xMoveContainer(pageX);
+
+      yMoveContainer(pageY);
+
+      xMoveCursor(pageX);
+
+      yMoveCursor(pageY);
+
+      xMoveCursorLabel(pageX);
+
+      yMoveCursorLabel(pageY);
+    });
+  }, []);
+
   return (
-    <>
+    <div>
       <motion.div
+        ref={modalContainer}
         variants={scaleAnimation}
         initial="initial"
         animate={active ? "enter" : "closed"}
@@ -44,12 +106,7 @@ export default function Modal({modal, projects}: props) {
                 style={{backgroundColor: color}}
                 key={`modal_${index}`}
               >
-                <Image
-                  src={`/images/${src}`}
-                  width={300}
-                  height={0}
-                  alt="image"
-                />
+                <Image src={src} width={300} height={0} alt="image" />
               </div>
             );
           })}
@@ -69,6 +126,6 @@ export default function Modal({modal, projects}: props) {
       >
         View
       </motion.div>
-    </>
+    </div>
   );
 }
