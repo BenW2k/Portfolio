e6d5d02437511390c4cbb6b999431e00bab5c331
BenW2k <benworkman16@gmail.com>
2024-07-08 08:04:23 +0100

    +navbar component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import "./styles/global.css";
+import Navbar from "./components/navbar";
 
 const inter = {subsets: ["latin"]};
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Navbar />
       <body>{children}</body>
     </html>
   );
