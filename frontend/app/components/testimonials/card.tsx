a4d93d10a3d3ffb01ee36f9942b863e2816940f7
BenW2k <benworkman16@gmail.com>
2024-07-15 17:12:18 +0100

    +structure changes

--- frontend/app/components/testimonials/card.tsx
+++ frontend/app/components/testimonials/card.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useRef} from "react";
+import quote from "./quotes.svg";
+import Image from "next/image";
 import styles from "../../styles/components/testimonials/card.module.css";
-import {motion, useTransform} from "framer-motion";
+import {motion, useInView, useTransform} from "framer-motion";
 
 interface Card {
   quote: string;
@@ -16,26 +18,63 @@ interface Card {
 
 export default function Card(card: Card) {
   const scale = useTransform(card.progress, card.range, [1, card.targetScale]);
+
   return (
     <div className={styles.cardContainer}>
       <motion.div
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        viewport={{amount: "all"}}
         className={styles.card}
-        style={{
-          scale,
-          backgroundColor: card.color,
-          top: `calc(-10% + ${card.i * 25}px)`,
-        }}
       >
         <div className={styles["card-contents"]}>
-          <p className={styles.quote}>{card.quote}</p>
-          <div className={styles["card-bottom"]}>
-            <div className={styles.from}>
-              <p className={styles.person}>{card.person}</p>
-              <p className={styles.company}>{card.company}</p>
+          <motion.div
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            viewport={{amount: "all"}}
+            className={styles["card-text"]}
+          >
+            <p className={styles.quote}>{card.quote}</p>
+            <div className={styles["card-bottom"]}>
+              <div className={styles.from}>
+                <p className={styles.person}>{card.person}</p>
+                <p className={styles.company}>{card.company}</p>
+              </div>
             </div>
-          </div>
+          </motion.div>
+          <motion.div
+            className={styles["image-container"]}
+            style={{
+              scale,
+              // backgroundColor: card.color,
+              top: `calc(-10% + ${card.i * 25}px)`,
+            }}
+          >
+            <Image src={card.img} alt="image" className={styles.image} />
+          </motion.div>
         </div>
       </motion.div>
     </div>
   );
+}
+
+{
+  /* <motion.div
+className={styles.card}
+style={{
+  scale,
+  backgroundColor: card.color,
+  top: `calc(-10% + ${card.i * 25}px)`,
+}}
+>
+<div className={styles["card-contents"]}>
+  <p className={styles.quote}>{card.quote}</p>
+  <div className={styles["card-bottom"]}>
+    <div className={styles.from}>
+      <p className={styles.person}>{card.person}</p>
+      <p className={styles.company}>{card.company}</p>
+    </div>
+  </div>
+</div>
+</motion.div> */
 }
