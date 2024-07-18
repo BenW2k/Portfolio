5951eaf9a9a1aa006fd48256ea00b33d33500f26
BenW2k <benworkman16@gmail.com>
2024-07-18 12:47:50 +0100

    +nav routing data - initial commit

---
+++ frontend/app/components/navbar/nav-links.ts
@@ -0,0 +1,37 @@
+export const links = [
+  {
+    title: "Work",
+    href: "/",
+  },
+  {
+    title: "Timeline",
+    href: "/",
+  },
+  {
+    title: "Skills",
+    href: "/",
+  },
+  {
+    title: "Forgot",
+    href: "/",
+  },
+  {
+    title: "Contact",
+    href: "/",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "GitHub",
+    href: "https://www.github.com/benw2k",
+  },
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/ben-workman-ds/",
+  },
+  // {
+  //   title: "Instagram",
+  //   href: "/",
+  // },
+];
