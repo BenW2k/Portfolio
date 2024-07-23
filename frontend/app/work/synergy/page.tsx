826d3ecb9110b23959e9108685c36f1b303f502b
BenW2k <benworkman16@gmail.com>
2024-07-23 11:47:28 +0100

    +images, video and paragraph changes

--- frontend/app/work/synergy/page.tsx
+++ frontend/app/work/synergy/page.tsx
@@ -6,17 +6,23 @@ import img from "../../assets/media/placeholder-image.svg";
 export default function Synergy() {
   return (
     <div className={styles.content}>
+      <div className={styles.header}>
+        <video autoPlay muted loop className={styles["video-background"]}>
+          <source src="/red_video.mp4" type="video/mp4" />
+        </video>
+        <h1 className={styles["page-title"]}>Attendance Tracker</h1>
+      </div>
       <Image src={img} className={styles.img1} alt="image" />
       <div className={styles.container1}>
         <div className={styles.section1}>
           <h3 className={styles["section-title"]}>Mission</h3>
           <div className={styles["section-body"]}>
             <p>
-              Create a web application to track attendance data of workers at
+              ▹ Create a web application to track attendance data of workers at
               Synergy Construction.
             </p>
             <p>
-              Client was using traditional paper sign-in sheets creating
+              ▹ Client was using traditional paper sign-in sheets creating
               difficulties surrounding long-term attendance tracking and data
               upkeep.
             </p>
@@ -25,10 +31,10 @@ export default function Synergy() {
         <div className={styles.section1}>
           <h3 className={styles["section-title"]}>Technologies</h3>
           <div className={styles["section-body"]}>
-            <p>Frontend: Typescript (React), Sass, HTML </p>
-            <p>Backend: Python (Django), SQL (MySQL)</p>
+            <p>▹ Frontend: Typescript (React), Sass, HTML </p>
+            <p>▹ Backend: Python (Django), SQL (MySQL)</p>
             <p>
-              Skills: API routing, Database Management, Automation, System
+              ▹ Skills: API routing, Database Management, Automation, System
               Design{" "}
             </p>
           </div>
@@ -39,17 +45,17 @@ export default function Synergy() {
         <div className={styles.section2}>
           <h3 className={styles["section-title"]}>Features</h3>
           <div className={styles["section-body"]}>
-            <p>Sign-in/Sign-out page for workers</p>
-            <p>Admin login with analytics dashboard</p>
-            <p>Weekly data back-ups</p>
+            <p>▹ Sign-in/Sign-out page for workers</p>
+            <p>▹ Admin login with analytics dashboard</p>
+            <p>▹ Weekly data back-ups</p>
           </div>
         </div>
         <div className={styles.section2}>
           <h3 className={styles["section-title"]}>Outcome</h3>
           <div className={styles["section-body"]}>
             <p> </p>
-            <p>Backend: </p>
-            <p>Skills: </p>
+            <p>▹ Backend: </p>
+            <p>▹ Skills: </p>
           </div>
         </div>
       </div>
