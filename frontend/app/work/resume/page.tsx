fd6d36f222526e4664af6928052766f6fd80ea7e
BenW2k <benworkman16@gmail.com>
2024-07-23 11:47:32 +0100

    +images, video and paragraph changes

--- frontend/app/work/resume/page.tsx
+++ frontend/app/work/resume/page.tsx
@@ -8,6 +8,9 @@ export default function Synergy() {
   return (
     <div className={styles.content}>
       <div className={styles.header}>
+        <video autoPlay muted loop className={styles["video-background"]}>
+          <source src="/green_video.mp4" type="video/mp4" />
+        </video>
         <h1 className={styles["page-title"]}>Level Designer Portfolio</h1>
       </div>
       <Image src={img} className={styles.img1} alt="image" />
@@ -16,12 +19,12 @@ export default function Synergy() {
           <h3 className={styles["section-title"]}>Mission</h3>
           <div className={styles["section-body"]}>
             <p>
-              Create a professional and presentable responsive personal
+              ▹ Create a professional and presentable responsive personal
               portfolio for a new university graduate. Aim was to highlight his
               skills in level-design and education.
             </p>
             <p>
-              Client needed a way to showcase his previous works to recruiters
+              ▹ Client needed a way to showcase his previous works to recruiters
               with a clean design but had no knowledge of web-development.
             </p>
           </div>
@@ -29,8 +32,8 @@ export default function Synergy() {
         <div className={styles.section1}>
           <h3 className={styles["section-title"]}>Technologies</h3>
           <div className={styles["section-body"]}>
-            <p>Frontend: Javascript (React), CSS, HTML </p>
-            <p>Skills: Web Design, Web Development, Web Hosting</p>
+            <p>▹ Frontend: Javascript (React), CSS, HTML </p>
+            <p>▹ Skills: Web Design, Web Development, Web Hosting</p>
           </div>
         </div>
       </div>
@@ -39,9 +42,9 @@ export default function Synergy() {
         <div className={styles.section2}>
           <h3 className={styles["section-title"]}>Features</h3>
           <div className={styles["section-body"]}>
-            <p>5 page fully responsive layout</p>
-            <p>Functional contact form using EmailJS API</p>
-            <p>Scroll and hover animations</p>
+            <p>▹ 5 page fully responsive layout</p>
+            <p>▹ Functional contact form using EmailJS API</p>
+            <p>▹ Scroll and hover animations</p>
           </div>
         </div>
         <div className={styles.section2}>
@@ -49,11 +52,11 @@ export default function Synergy() {
           <div className={styles["section-body"]}>
             <p> </p>
             <p>
-              Client entirely satisfied and has communicated that he has already
-              received interest through the contact form.
+              ▹ Client entirely satisfied and has communicated that he has
+              already received interest through the contact form.
             </p>
             <p>
-              Client also left a positive testimonial and has expressed his
+              ▹ Client also left a positive testimonial and has expressed his
               desire to work together in the future.{" "}
             </p>
           </div>
