5f5039001e788a54e5d190f2e7c8aa07491710ce
BenW2k <benworkman16@gmail.com>
2024-07-24 18:14:40 +0100

    -removed skill slider

--- frontend/app/components/about.tsx
+++ frontend/app/components/about.tsx
@@ -44,9 +44,6 @@ export default function About() {
           </div>
           <div className={`${styles["glass"]}  ${styles["dummy"]} `}>Dummy</div>
         </div>
-        <div className={styles["logo-slider"]}>
-          <SkillSlider />
-        </div>
       </div>
     </div>
   );
