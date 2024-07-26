c90b84a2a89cb9178cd68eb833b93440c4425165
BenW2k <benworkman16@gmail.com>
2024-07-26 11:58:35 +0100

    +changed card colour + imgs

--- frontend/app/components/testimonials/data.ts
+++ frontend/app/components/testimonials/data.ts
@@ -1,12 +1,14 @@
 import placeholder from "../../assets/media/placeholder-image.svg";
 import test from "../../assets/media/face-nobg.png";
+import connor from "../../assets/media/connor_test.svg";
+import tnp from "../../assets/media/tnp_test.svg";
 
 export const testimonials = [
   {
     quote:
       "First ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    img: placeholder,
-    color: "#ABABAB",
+    img: connor,
+    color: "white",
     person: "Mark",
     company: "Synergy Construction",
     i: 1,
@@ -14,8 +16,8 @@ export const testimonials = [
   {
     quote:
       "Ben provided great communication throughout the project and always had my interests in mind. Alongside this, he completed the work in a very timely manner and allowed for apt time for revisions. Ben's professionalism and passion made him a joy to work with.",
-    img: placeholder,
-    color: "rgba(216, 223, 255, 1)",
+    img: connor,
+    color: "white",
     person: "Connor Larkin ",
     company: "Glasgow Caledonian University",
     i: 2,
@@ -23,8 +25,8 @@ export const testimonials = [
   {
     quote:
       "Third ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    img: placeholder,
-    color: "rgba(195, 205, 255, 1)",
+    img: tnp,
+    color: "white",
     person: "Placeholder Name",
     company: "Placeholder Company",
     i: 3,
