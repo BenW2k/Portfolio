dc557c4fa89f6e60d54f21ae2ca3e1eb716a87da
BenW2k <benworkman16@gmail.com>
2024-08-24 12:45:12 +0100

    +initial commit

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  // Any other Next.js configuration options
+};
+
 export default nextConfig;
