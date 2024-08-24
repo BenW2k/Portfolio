df7a200d59dc0a107086d2d6969f00690b6f65bd
BenW2k <benworkman16@gmail.com>
2024-08-24 12:57:12 +0100

    -removed conflicting code

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// next.config.js
-module.exports = {
-  reactStrictMode: true,
-  // Any other Next.js configuration options
-};
-
 export default nextConfig;
